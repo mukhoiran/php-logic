35bd049f56718a87ecb5097dad8b0aed27ec29d6
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-31 11:26:24 +0700

    fix

--- highest.php
+++ highest.php
@@ -1,11 +1,16 @@
 <?php
-$array = array(11, 6, 31, 201, 99, 861, 1, 7, 14, 79);
-$highest = 0;
-
-foreach($array as $item){
-    if ($item > $highest){
-        $highest = $item;
-    }
+//Function for get highest value in array
+function getHighest($array){
+  $highest = 0;
+  foreach($array as $item){
+      if ($item > $highest){
+          $highest = $item;
+      }
+  }
+  echo $highest;
 }
-echo $highest;
+
+//Array data
+$data = array(11, 6, 31, 201, 99, 861, 1, 7, 14, 79);
+getHighest($data);
 ?>
--- prime.php
+++ prime.php
@@ -1,10 +1,13 @@
 <?php
+//Function for get prime number
 function isPrime($n) {
     for($x=2; $x<$n; $x++) {
       if($n % $x == 0) { return false;}
     }
     return true;
 }
+
+//input number
 $number = 17;
 $check = isPrime($number);
 echo $check ? $number.' is a Prime Number.' :  $number.' is not a Prime Number.';
